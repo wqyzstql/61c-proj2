cbbcfd6161e0f63ee2aeb6fb226f018c10114ac8
wqyzstql <1139198820@qq.com>
2024-08-19 10:56:41 +0800

    Task1 Finished

--- src/abs.s
+++ src/abs.s
@@ -1,18 +1,17 @@
 .globl abs
 
 .text
-# =================================================================
-# FUNCTION: Given an int return its absolute value.
-# Arguments:
-#   a0 (int*) is a pointer to the input integer
-# Returns:
-#   None
-# =================================================================
 abs:
-    # Prologue
-
-    # PASTE HERE
-
-    # Epilogue
-
+    addi sp, sp, -4
+    sw t0, 0(sp)
+    # Load number from memory
+    lw t0 0(a0)
+    bge t0, zero, done
+    # Negate a0
+    sub t0, x0, t0
+    # Store number back to memory
+    sw t0 0(a0)
+done:
+    lw t0, 0(sp)
+    addi sp, sp, 4
     jr ra
